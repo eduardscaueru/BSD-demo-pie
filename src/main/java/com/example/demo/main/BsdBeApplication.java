8444e2caefe2f16c787436eb36249e683a26ae5e
catalinpicior <catalin.picior@gmail.com>
2024-01-14 00:42:17 +0200

    catalin.picior - added script to init db

---
+++ src/main/java/com/example/demo/main/BsdBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsdBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsdBeApplication.class, args);
+	}
+
+}
